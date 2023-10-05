2e8f2e8ce54b63ef24b55684eb992b55943fb3df
Jack Tran <bluestar113311@gmail.com>
2023-10-04 20:06:41 -0700

    Add database schema and seed

---
+++ app/api/db/add-scenario/route.ts
@@ -0,0 +1,19 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const title = searchParams.get('title');
+  const description = searchParams.get('description');
+  const campaign_id = searchParams.get('campaign_id');
+ 
+  try {
+    if (!title) throw new Error('Scenario title required');
+    await sql`INSERT INTO scenarios (title, description, enemies, campaign_id) VALUES (${title}, ${description}, ${campaign_id});`;
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+ 
+  const scenarios = await sql`SELECT * FROM scenarios;`;
+  return NextResponse.json({ scenarios }, { status: 200 });
+}
