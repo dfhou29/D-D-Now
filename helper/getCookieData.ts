f2446a8ae5e9ff6e1c7803f388120e5fa262cd1e
Jack Tran <bluestar113311@gmail.com>
2023-10-09 21:10:03 -0700

    Add helper for retrieving cookie

---
+++ helper/getCookieData.ts
@@ -0,0 +1,13 @@
+import { cookies } from 'next/headers'
+import jwt from "jsonwebtoken";
+
+export const getCookieData = () => {
+  try {
+    const cookieStore = cookies()
+    const token = cookieStore.get("token")?.value || '';
+    const decodedToken:any = jwt.verify(token, process.env.TOKEN_SECRET!);
+    return decodedToken;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+}
