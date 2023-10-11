28c1c5209800096c46836a59a3dcd283c2b2d5c0
Jack Tran <bluestar113311@gmail.com>
2023-10-10 22:38:26 -0700

    Update setting and scenario table to cascade on campaign delete

--- app/api/db/create-setting-table/route.ts
+++ app/api/db/create-setting-table/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   try {
     const result =
-      await sql`CREATE TABLE settings (id SERIAL PRIMARY KEY NOT NULL, title VARCHAR(255) NOT NULL, description TEXT, campaign_id INTEGER REFERENCES campaigns(id) NOT NULL);`;
+      await sql`CREATE TABLE settings (id SERIAL PRIMARY KEY NOT NULL, title VARCHAR(255) NOT NULL, description TEXT, campaign_id INTEGER NOT NULL REFERENCES campaigns(id) ON DELETE CASCADE);`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
